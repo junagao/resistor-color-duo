c2bc1a20d2003260747fbc6d0600c7a407f9e713
Juliane Nagao <julianens@gmail.com>
2019-07-27 10:48:30 +0200

    fix: solution

--- resistor-color-duo.js
+++ resistor-color-duo.js
@@ -1,3 +1,3 @@
-export const value = colors => Number(colors.map(color => Number(COLORS.indexOf(color))).join(''));
+export const value = colors => Number(colors.map(color => COLORS.indexOf(color)).join(''));
 
 const COLORS = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
